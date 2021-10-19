99f84bf882c974f08aca0541b8f20601e16b163f
LinusPotocnik <linuspotocnik@icloud.com>
2021-10-20 00:24:09 +0200

    initial commit

---
+++ events/interactionCreate.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+module.exports = {
+	name: 'interactionCreate',
+	execute(interaction) {
+		for(file of commandFiles) {
+			if(file.substring(0, (file.length-3)) == interaction.commandName) {
+			const command = require(__dirname + `/../commands/${file}`);
+			command.execute(interaction)
+			}
+		}
+	},
+};
---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const { Client, Collection, Intents } = require('discord.js')
+require('dotenv').config()
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS]})
+
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.login(process.env.BOT_TOKEN)
