c2d674acc5d41d492e6b3a2dc4ba019db98c618f
LinusPotocnik <linuspotocnik@icloud.com>
2021-10-20 22:11:53 +0200

    Create color.js

---
+++ commands/color.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    // Creates a new SlashCommand
+    data: new SlashCommandBuilder()
+        .setName('color')
+        .setDescription('Sends a picture with the given color!')
+        .addStringOption(option =>
+            option.setName('hex')
+                .setDescription('The hex code of the color you want displayed. Please use the format #RRGGBB')
+                .setRequired(true)),
+    async execute(interaction) {
+        try {
+            // Save color as #RRGGBB, regardless of the color being in the format #RRGGBB or just RRGGBB
+            const color = '#' + (interaction.options.getString('hex').startsWith('#') ? interaction.options.getString('hex').substring(1) : interaction.options.getString('hex'))
+
+            // Creates an MessageEmbed with the #RRGGBB as a title and a picture/surface with that color
+            const colorEmbed = new MessageEmbed()
+                .setTitle(color)
+                .setColor(color)
+                .setThumbnail("https://singlecolorimage.com/get/" + color.substring(1) + "/400x400")
+
+            //Sends that MessageEmbed
+            interaction.reply({ embeds: [colorEmbed] })
+        } catch (error) {
+            // Catches any formatting mistakes by the executor and returns an error
+            console.error(error);
+            interaction.reply('Hopla, da ist etwas schief gelaufen!')
+        }
+    }
+}
