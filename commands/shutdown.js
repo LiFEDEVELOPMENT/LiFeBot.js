8e0b50517c160175369544550b7a14879d4f14a4
LinusPotocnik <linuspotocnik@icloud.com>
2021-10-21 22:29:04 +0200

    Create shutdown.js
    Note: No permissions required

---
+++ commands/shutdown.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require("@discordjs/builders")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('shutdown')
+    .setDescription('Fährt den Bot herunter!'),
+    async execute(interaction) {
+        await interaction.reply('Der Bot wird heruntergefahren!');
+        interaction.client.destroy();
+    }
+}
