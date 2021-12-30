e7bf7535e7da8d37607f50f88ae4c751fb456e32
LinusPotocnik <linuspotocnik@icloud.com>
2021-12-31 00:08:59 +0100

    Format commands; change sqlite library again lol

--- commands/announce.js
+++ commands/announce.js
@@ -2,28 +2,43 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    // Creates a new SlashCommand
-    data: new SlashCommandBuilder()
-        .setName('announce')
-        .setDescription('Announced eine Nachricht!')
-        .addStringOption(option =>
-            option.setName('message')
-                .setDescription('Die Nachricht, die announced werden soll')
-                .setRequired(true))
-        .addRoleOption(option =>
-            option.setName('role')
-                .setDescription('Die Rolle, die in der Nachricht erwähnt werden soll')
-                .setRequired(false)),
-    async execute(interaction) {
-        // Prepare MessageEmbed for the announce
-        const announceEmbed = new MessageEmbed()
-            .setColor("YELLOW")
-            .setTitle('Announce')
-            .setDescription((interaction.options.getRole('role') != null ? "<@&" + interaction.options.getRole('role') + "> \n\n" : "") + interaction.options.getString('message'))
-            .setFooter(interaction.member.nickname != null ? `${interaction.member.nickname}` : `${interaction.member.user.username}`);
+	// Creates a new SlashCommand
+	data: new SlashCommandBuilder()
+		.setName('announce')
+		.setDescription('Announced eine Nachricht!')
+		.addStringOption((option) =>
+			option
+				.setName('message')
+				.setDescription('Die Nachricht, die announced werden soll')
+				.setRequired(true)
+		)
+		.addRoleOption((option) =>
+			option
+				.setName('role')
+				.setDescription('Die Rolle, die in der Nachricht erwähnt werden soll')
+				.setRequired(false)
+		),
+	async execute(interaction) {
+		// Prepare MessageEmbed for the announce
+		const announceEmbed = new MessageEmbed()
+			.setColor('YELLOW')
+			.setTitle('Announce')
+			.setDescription(
+				(interaction.options.getRole('role') != null
+					? '<@&' + interaction.options.getRole('role') + '> \n\n'
+					: '') + interaction.options.getString('message')
+			)
+			.setFooter(
+				interaction.member.nickname != null
+					? `${interaction.member.nickname}`
+					: `${interaction.member.user.username}`
+			);
 
-        // Send the MessageEmbed and reply with a confirmation for the executor
-        await interaction.channel.send({ embeds: [announceEmbed] });
-        await interaction.reply({ content: 'Die Nachricht wurde announced!', ephemeral: true });
-    },
+		// Send the MessageEmbed and reply with a confirmation for the executor
+		await interaction.channel.send({ embeds: [announceEmbed] });
+		await interaction.reply({
+			content: 'Die Nachricht wurde announced!',
+			ephemeral: true,
+		});
+	},
 };
