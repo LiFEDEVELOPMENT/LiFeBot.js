ed435d74c73ea558014e12020c4bfde08b0a96be
LinusPotocnik <linuspotocnik@icloud.com>
2022-01-17 03:02:43 +0100

    Rename maxAntworten to maxAnswers

--- utility/Utilities.js
+++ utility/Utilities.js
@@ -3,7 +3,7 @@ import sql from '#sql';
 async function registerNewPoll(
 	guildid,
 	authorid,
-	maxAntworten,
+	maxAnswers,
 	frage,
 	antwort1,
 	antwort2,
@@ -21,7 +21,7 @@ async function registerNewPoll(
 
 	// Prepaare sql statement and run it with prepared arguments
 	let preparedSQL =
-		'INSERT INTO polls (guildid,authorid,maxAntworten,frage,antwort1,antwort2,antwort3,antwort4,antwort5,antwort6,antwort7,antwort8,antwort9,antwort10) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
+		'INSERT INTO polls (guildid,authorid,maxAnswers,frage,antwort1,antwort2,antwort3,antwort4,antwort5,antwort6,antwort7,antwort8,antwort9,antwort10) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)';
 	await sql.run(preparedSQL, sqlParams);
 
 	// Return the id of the newly generated poll-entry
