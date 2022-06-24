78fe9823a8049a58772b12a24364808b1734ba31
LinusPotocnik <linuspotocnik@icloud.com>
2022-06-24 15:20:25 +0200

    Removed some testing-clutter

--- events/interactions/commands/voice.js
+++ events/interactions/commands/voice.js
@@ -21,12 +21,6 @@ async function create() {
 
 	command.addSubcommand(
 		new SlashCommandSubcommandBuilder()
-			.setName('join')
-			.setDescription('Joins the voice channel you are currently in')
-	);
-
-	command.addSubcommand(
-		new SlashCommandSubcommandBuilder()
 			.setName('leave')
 			.setDescription('Leaves the voice channel if it is in your voice channel')
 	);
@@ -58,9 +52,6 @@ async function execute(interaction) {
 			case 'play':
 				answer = await playCommand(interaction);
 				break;
-			case 'join':
-				answer = await joinCommand(interaction);
-				break;
 			case 'leave':
 				answer = await leaveCommand(interaction);
 		}
@@ -91,35 +82,6 @@ async function playCommand(interaction) {
 	} catch (error) {
 		console.log(error);
 	}
-}
-
-async function joinCommand(interaction) {
-	const locale = interaction.locale;
-
-	const connection = joinVoiceChannel({
-		channelId: interaction.member.voice.channel.id,
-		guildId: interaction.guild.id,
-		adapterCreator: interaction.guild.voiceAdapterCreator,
-		selfDeaf: true,
-	});
-
-	const player = createAudioPlayer();
-
-	const __filename = fileURLToPath(import.meta.url);
-	const __dirname = path.dirname(__filename);
-
-	// const resource = createAudioResource(
-	// 	path.join(__dirname, '../../../utility/Song.mp3')
-	// );
-	const resource = createAudioResource(
-		await queue.stream('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-	);
-
-	player.play(resource);
-
-	connection.subscribe(player);
-
-	return await lang('VOICE_JOIN_SUCCESS', {}, locale);
 }
 
 async function leaveCommand(interaction) {
